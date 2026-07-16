ae352baa916e96c3d8f6b46fde2d428092653bd6
xaxaaxaaa-hash <xaxaaxaaa@gmail.com>
2026-07-16 18:28:17 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
-# Установка базовых пакетов
+# Обновление базовых пакетов (быстрое)+русификация
 apk update && apk upgrade && apk add luci-i18n-base-ru || {
-    echo "Ошибка установки базовых пакетов"
+    echo "Ошибка установки базовых пакетов. WAN подключен? Ебало провайдера представили?"
     exit 1
 }
 
-# Установка темы Aurora
+# Скачивание и установка темы Aurora
 cd /tmp || exit 1
 uclient-fetch -O luci-theme-aurora.apk https://github.com/eamonxg/luci-theme-aurora/releases/latest/download/luci-theme-aurora-1.1.0-r20260711.apk || {
-    echo "Ошибка загрузки темы Aurora"
+    echo "Ошибка загрузки темы Aurora. Интернет есть?"
     exit 1
 }
 apk add --allow-untrusted luci-theme-aurora.apk || {
@@ -17,7 +17,7 @@ apk add --allow-untrusted luci-theme-aurora.apk || {
 
 # Установка sing-box-extended
 uclient-fetch -O sing-box-extended.apk https://github.com/shtorm-7/sing-box-extended/releases/download/v1.13.14-extended-2.5.1/sing-box-extended_1.13.14-extended-2.5.1_openwrt_aarch64_cortex-a53.apk || {
-    echo "Ошибка загрузки sing-box-extended"
+    echo "Ошибка загрузки sing-box-extended. Есть 22 мегабайта?"
     exit 1
 }
 apk add --allow-untrusted sing-box-extended.apk || {
